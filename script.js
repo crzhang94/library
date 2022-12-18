61835a54a2260b403124079aa0f000b014822494
Christine <110146143+crzhang94@users.noreply.github.com>
2022-12-18 15:07:07 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
