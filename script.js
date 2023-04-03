24fbe9c28d7fef637ef138344275d6a993dfc130
Christine <110146143+crzhang94@users.noreply.github.com>
2023-04-02 21:33:21 -0700

    Add function for displaying books on the page

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-
-// Empty array for storing book objects
 let myLibrary = [];
 
-// Constructor for making book objects
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-    // function that can report the book info (from exercise)
-    /* this.info = function() {
-      return title + " by " + author + ", " + pages + " pages, " + readStatus;
-    } */
 }
 
-// Function to store new book objects into library array
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
@@ -22,67 +14,44 @@ function addBookToLibrary() {
   let readStatus = document.getElementById("read-status").value;
 
   const newBook = new Book(title, author, pages, readStatus);
-  console.log(newBook);
-
-  // myLibrary.push(book);
+  myLibrary.push(newBook);
+  
+  displayBooks();
 }
 
-
-// Bring form up when add new book button is pressed
+// Bring form up when "add new book" button is pressed
 let newBookButton = document.querySelector("#add-button");
 newBookButton.addEventListener('click', function() {
   let newBookForm = document.querySelector("#book-form");
   newBookForm.style.display = "block";
 })
 
-// Form's add book button
+// Form: add book button
 document.querySelector("#book-form").addEventListener("submit", function() {
   event.preventDefault();
   addBookToLibrary();
 })
 
-
-// Form's cancel button
+// Form: cancel button
 document.getElementById("cancel").addEventListener("click", function () {
   let newBookForm = document.querySelector("#book-form");
   newBookForm.style.display = "none";
   newBookForm.reset();
 })
 
-
-// Sample books - currently 3 books in the library w/o user input
-addBookToLibrary('Cats', 'Me', 192, 'read');
-addBookToLibrary('Cats', 'Me', 1922, 'read');
-addBookToLibrary('Cats', 'Me', 1923, 'read');
-addBookToLibrary('Cats', 'Me', 19233, 'read');
-console.log(myLibrary.length);
-
-// Function that loops through array and displays each book on the page
 function displayBooks() {
-  const bookshelf = document.querySelector('.bookshelf');
-
-  // Loop through each book (element) of array
-  myLibrary.forEach(myLibrary => {
-    // Create a new card on the bookshelf for each book
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookshelf.appendChild(bookCard);
-    // For each book card, add text showing book info
-    for (let key in myLibrary) {
-      const info = document.createElement('p');
-      info.classList.add('book-info');
-      info.textContent = key + ": " + myLibrary[key];
-      console.log(key, myLibrary[key]);
-      bookCard.appendChild(info);
-    }
-  })
-
-  /* for (let i = 0; i < myLibrary.length; i++) {
-    const bookCard = document.createElement('div');
-    bookCard.className.add = ('book-card');
-    bookshelf.appendChild(bookCard);
-  } */
-
+  const bookshelf = document.querySelector(".bookshelf");
+  bookshelf.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      let book = myLibrary[i];
+      let bookCard = document.createElement("div");
+      bookCard.setAttribute("class", "book-card");
+      bookCard.innerHTML = `
+        <div class = "card-title"><b>${book.title}</b></div>
+        <div class = "card-author">${book.author}</div>
+        <div class = "card-pages">${book.pages} pages</div>
+        <div class = "card-read-status">${book.readStatus}</div>
+        `;
+      bookshelf.appendChild(bookCard);
+  }
 }
-
-displayBooks();
